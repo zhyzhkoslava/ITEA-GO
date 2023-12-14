3d6bc1e7ee44c54358b2542878e370bd9799863f
Viacheslav <zhizhkoslava@gmail.com>
2023-12-14 19:04:12 +0200

    fix hw11

--- main.go
+++ main.go
@@ -31,27 +31,6 @@ func readFile(filePath string, linesCh chan []string, wg *sync.WaitGroup) {
 	linesCh <- lines
 }
 
-func writeFile(outputFilePath string, linesCh chan []string, doneCh chan struct{}, wg *sync.WaitGroup) {
-	defer wg.Done()
-
-	outputFile, err := os.OpenFile(outputFilePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		fmt.Println("Error opening output file:", err)
-		return
-	}
-	defer outputFile.Close()
-
-	for lines := range linesCh {
-		for _, line := range lines {
-			_, err := fmt.Fprintln(outputFile, line)
-			if err != nil {
-				fmt.Println("Error writing to output file:", err)
-				return
-			}
-		}
-	}
-}
-
 func main() {
 	file1Path := "hw11/file1.txt"
 	file2Path := "hw11/file2.txt"
@@ -59,7 +38,6 @@ func main() {
 
 	var wg sync.WaitGroup
 	linesCh := make(chan []string, 2)
-	doneCh := make(chan struct{})
 
 	wg.Add(2)
 	go readFile(file1Path, linesCh, &wg)
@@ -68,9 +46,23 @@ func main() {
 	wg.Wait()
 	close(linesCh)
 
-	wg.Add(1)
-	go writeFile(outputFilePath, linesCh, doneCh, &wg)
+	var combinedLines []string
+	for lines := range linesCh {
+		combinedLines = append(combinedLines, lines...)
+	}
 
-	wg.Wait()
-	close(doneCh)
+	outputFile, err := os.Create(outputFilePath)
+	if err != nil {
+		fmt.Println("Error opening output file:", err)
+		return
+	}
+	defer outputFile.Close()
+
+	for _, line := range combinedLines {
+		_, err := fmt.Fprintln(outputFile, line)
+		if err != nil {
+			fmt.Println("Error writing to output file:", err)
+			return
+		}
+	}
 }
